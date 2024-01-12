25f6a28b20967613cee63ce1519d7cb8a8a2ccb5
MichelCode <michelseocode.com>
2024-01-12 16:05:52 +0100

    v4

--- database/migrations/2024_01_08_143237_create_categories_table.php
+++ database/migrations/2024_01_08_143237_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->name();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
