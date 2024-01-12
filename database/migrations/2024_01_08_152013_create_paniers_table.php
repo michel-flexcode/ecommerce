4903e3d5bc4f70f7c99ba9a4a093bc54780276c3
MichelCode <michelseocode.com>
2024-01-12 15:30:52 +0100

    v4

--- database/migrations/2024_01_08_152013_create_paniers_table.php
+++ database/migrations/2024_01_08_152013_create_paniers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('paniers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
             $table->timestamps();
         });
     }
