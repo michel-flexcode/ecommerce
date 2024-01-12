fb0ddd241dba9dbf44aa459053d282dc078a6481
MichelCode <michelseocode.com>
2024-01-12 16:11:42 +0100

    cusseeder

--- database/seeders/CustomersSeeder.php
+++ database/seeders/CustomersSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class CustomersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Exemple de données pour les clients
+        $customersData = [
+            ['user_id' => 1],
+            ['user_id' => 2],
+            ['user_id' => 3],
+            // Ajoutez d'autres clients au besoin
+        ];
+
+        // Boucle sur les données et les insère dans la table 'customers'
+        foreach ($customersData as $customerData) {
+            Customer::create($customerData);
+        }
     }
 }
