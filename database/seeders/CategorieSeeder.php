c82cea819af286216a6b6dfd5d3f5d89cc630eee
MichelCode <michelseocode.com>
2024-01-12 16:10:50 +0100

    catseeder

--- database/seeders/CategorieSeeder.php
+++ database/seeders/CategorieSeeder.php
@@ -13,6 +13,17 @@ class CategorieSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Exemple de données pour la catégorie
+        $categoriesData = [
+            ['name' => 'Catégorie 1', 'product_id' => 1],
+            ['name' => 'Catégorie 2', 'product_id' => 2],
+            ['name' => 'Catégorie 3', 'product_id' => 3],
+            // Ajoutez d'autres catégories au besoin
+        ];
+
+        // Boucle sur les données et les insère dans la table 'categories'
+        foreach ($categoriesData as $categoryData) {
+            Categorie::create($categoryData);
+        }
     }
 }
