48e36e63e01cf8e38cad9bed59f8fa29c9d343b2
MichelCode <michelseocode.com>
2024-01-12 14:58:54 +0100

    product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,10 @@ class ProductsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            // Add more attributes as needed for your Products model
         ];
     }
 }
