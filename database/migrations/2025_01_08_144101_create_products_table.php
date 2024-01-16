91c3892937a7f5571e29a8503620643e50906785
MichelCode <michelseocode.com>
2024-01-16 14:12:28 +0100

    v12

--- database/migrations/2025_01_08_144101_create_products_table.php
+++ database/migrations/2025_01_08_144101_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->foreignId('categorie_id')->constrained()->onDelete('cascade');
-            $table->integer('price');
+            $table->float('price');
             $table->timestamps();
         });
     }
