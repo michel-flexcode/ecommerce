4bcc60aeddf3ce683ffaf8ec662053fce7f8fd35
MichelCode <michelseocode.com>
2024-01-16 16:00:04 +0100

    Advise added

--- database/seeders/AdviseSeeder.php
+++ database/seeders/AdviseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Advise;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,28 @@ class AdviseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Exemple de création de plusieurs avis fictifs
+        $advises = [
+            [
+                'product_id' => 1,
+                'customer_id' => 1,
+                'content' => 'Ceci est un avis sur le produit 1.',
+            ],
+            [
+                'product_id' => 2,
+                'customer_id' => 2,
+                'content' => 'Ceci est un avis sur le produit 2.',
+            ],
+            [
+                'product_id' => 1,
+                'customer_id' => 3,
+                'content' => 'Ceci est un autre avis sur le produit 1.',
+            ],
+            // Ajoute d'autres avis selon tes besoins
+        ];
+
+        foreach ($advises as $advise) {
+            Advise::create($advise);
+        }
     }
 }
